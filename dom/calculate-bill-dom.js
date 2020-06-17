d706e88c03b5f24b191a229774bee35629787704
Si-hub <zweto.dayile@gmail.com>
2020-06-17 15:07:19 +0200

    added some files in my repo

--- dom/calculate-bill-dom.js
+++ dom/calculate-bill-dom.js
@@ -7,47 +7,26 @@ const calculateBtn = document.querySelector(".calculateBtn");
 
 const billTotalSpanElement = document.querySelector(".total");
 
-function totalPhoneBill(billString) {
 
+var calculateInstance = calculateBillEvent(billString)
 
-//split the string
-    var billItems = billString.split(",");
-    // a variable for the total phone bill.
-    var billTotal = 0;
-    //loop over all the bill items
-    for (var i=0;i<billItems.length;i++){
-        var billItem = billItems[i].trim();
+function totalPhoneBill() {
+
         if (billItem === "call"){
-            billTotal += 2.75;
+            calculateInstance.enteredCall();
         }
         else if (billItem === "sms"){
-            billTotal += 0.75;
+            calculateInstance.enteredSms();
         }
     }
     
-    //round to two decimals
-    var roundedBillTotal = billTotal.toFixed(2);
-return roundedBillTotal;
-
-} 
-function styleTotalColor(roundedBillTotal)  {
-
-const currentTotal = Number(roundedBillTotal);
+ 
+function styleTotalColor()  {
 
 
 billTotalSpanElement.classList.remove("danger");
 billTotalSpanElement.classList.remove("warning");
-
-//color the total based on the criteria
-    if (currentTotal  >= 30){
-        // adding the danger class will make the text red
-       billTotalSpanElement.classList.add("danger");
-    }
-    else if (currentTotal >= 20 && currentTotal < 30){
-       billTotalSpanElement.classList.add("warning");
-    
-
-} 
+billTotalSpanElement.classList.add(calculateInstance.calculateTotalClassName());
 
 };
 
@@ -55,23 +34,14 @@ function calculateBtnClicked () {
 var billString = billStringTotalElement.value;
 
 
-  const roundedBillTotal = totalPhoneBill(billString);
+billTotalElement.innerHTML = calculateInstance.getCalculateTotal().toFixed(2);
 
-
-billTotalElement.innerHTML = roundedBillTotal;  
-styleTotalColor(roundedBillTotal);
-
-    billTotalElement.innerHTML = roundedBillTotal;
+    //billTotalElement.innerHTML = roundedBillTotal;
 
 };
 
 //link the function to a click event on the calculate button
 calculateBtn.addEventListener("click", calculateBtnClicked);
 
-//create the function that will be called when the calculate button is pressed
-//  * this function should read the string value entered - split it on a comma.
-//  * loop over all the entries in the the resulting list
-//  * check if it is a call or an sms and add the right amount to the overall total
-//  * once done looping over all the entries - display the total onto the screen in the billTotal element
 
 
